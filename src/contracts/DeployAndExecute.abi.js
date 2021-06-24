a2da297768e9f6f8977ec8272e25aad05cc25b52
Kyle <the.big.kb123@gmail.com>
2021-06-24 12:49:46 -0400

    Updating bytecode of abi contracts

--- src/contracts/DeployAndExecute.abi.js
+++ src/contracts/DeployAndExecute.abi.js
@@ -1,36 +1,41 @@
 module.exports = [
-  {
-    "inputs": [
-      {
-        "internalType": "contract ISingletonFactory",
-        "name": "_singletonFactory",
-        "type": "address"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "bytes",
-        "name": "initCode",
-        "type": "bytes"
-      },
-      {
-        "internalType": "bytes32",
-        "name": "salt",
-        "type": "bytes32"
-      },
-      {
-        "internalType": "bytes",
-        "name": "execData",
-        "type": "bytes"
-      }
-    ],
-    "name": "deployAndExecute",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }
-]
+    {
+      "inputs": [
+        {
+          "internalType": "contract ISingletonFactory",
+          "name": "_singletonFactory",
+          "type": "address"
+        },
+        {
+          "internalType": "contract ExecutorAccessController",
+          "name": "_executorAccessController",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "bytes",
+          "name": "initCode",
+          "type": "bytes"
+        },
+        {
+          "internalType": "bytes32",
+          "name": "salt",
+          "type": "bytes32"
+        },
+        {
+          "internalType": "bytes",
+          "name": "execData",
+          "type": "bytes"
+        }
+      ],
+      "name": "deployAndExecute",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ]
