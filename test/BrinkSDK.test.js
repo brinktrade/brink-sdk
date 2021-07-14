ca5730a1d8d3eca0c3bc5e81e426195ccff19257
Kyle <the.big.kb123@gmail.com>
2021-07-14 15:38:06 -0400

    Removing unused test file

--- test/BrinkSDK.test.js
+++
@@ -1,9 +0,0 @@
-
-// const { BrinkSDK } = require('../src')
-
-// describe.only('BrinkSDK', function () {
-//   beforeEach(async function () {
-//     const sdk = new BrinkSDK(configuration)
-//     const account = sdk.newAccount({ ethersSigner, signer })
-//   })
-// })
