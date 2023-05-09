26b1a0728136f0550771f64d4c6a04402de3edbc
Mike Calvanese <mfcalvanese@gmail.com>
2023-05-09 09:47:13 -0400

    update externalCall fn sig

--- test/local/account/externalCall.test.ts
+++ test/local/account/externalCall.test.ts
@@ -10,7 +10,11 @@ describe('externalCall', function () {
 
   it('should send externalCall tx', async function () {
     const transferAmount = BigInt((await ethers.utils.parseEther('0.01')).toString())
-    const tx = await externalCall(this.accountAddress, transferAmount, this.recipientAddress, '0x')
+    const tx = await externalCall({
+      signer: this.signerAddress,
+      value: transferAmount,
+      to: this.recipientAddress
+    })
     const receipt = await this.ethersAccountSigner.sendTransaction(tx)
     expect(receipt).to.not.be.undefined
     expect((await ethers.provider.getBalance(this.recipientAddress)).toString()).to.equal(ethers.utils.parseEther('0.01').toString())
