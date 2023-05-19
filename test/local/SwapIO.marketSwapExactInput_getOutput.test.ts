adb8217bc3bd15a2d7788f7b6cf052218eaaa018
Mike Calvanese <mfcalvanese@gmail.com>
2023-05-19 09:42:30 -0400

    update price in SwapIO.marketSwapExactInput_getOutput test

--- test/local/SwapIO.marketSwapExactInput_getOutput.test.ts
+++ test/local/SwapIO.marketSwapExactInput_getOutput.test.ts
@@ -5,12 +5,12 @@ describe('marketSwapExactInput_getOutput', function () {
   it('should return output and fee data', async function () {
     const res = await marketSwapExactInput_getOutput({
       input: BigInt(1800) * BigInt(10)**BigInt(18),
-      priceX96: BigInt(1500) * BigInt(2)**BigInt(96),
+      priceX96: BigInt(2)**BigInt(96) / BigInt(1500),
       feePercent: BigInt(5) * BigInt(10)**BigInt(4),
       feeMin: BigInt(0)
     })
-    expect(res.output.toString()).to.equal('2700000000000000000000000')
-    expect(res.fee.toString()).to.equal('135000000000000000000000')
-    expect(res.outputWithFee.toString()).to.equal('2565000000000000000000000')
+    expect(res.output.toString()).to.equal('1199999999999999999')
+    expect(res.fee.toString()).to.equal('59999999999999999')
+    expect(res.outputWithFee.toString()).to.equal('1140000000000000000')
   })
 })
