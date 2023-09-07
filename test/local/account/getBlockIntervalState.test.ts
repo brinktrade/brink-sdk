fd6436b783734de3c4e04dd455f4e5807c9a274a
Mati OS <matias.ovejasmith@gmail.com>
2023-09-07 18:04:02 -0300

    update getBlockIntervalState() tests

--- test/local/account/getBlockIntervalState.test.ts
+++ test/local/account/getBlockIntervalState.test.ts
@@ -2,12 +2,24 @@ import { expect } from 'chai'
 import { getBlockIntervalState } from '@brink-sdk'
 
 describe('getBolckIntervalState()', function () {
-  it('when given bit is used, should return true', function () {
-    
-    const slot = 12345n
+  it('when slot is not used, should return 0', function () {
+    const slot = 0
     const { counter, start } = getBlockIntervalState({ slot })
-
     expect(counter).to.equal(0)
     expect(start).to.equal(slot)
+  })
+
+  it("when is all 1's, should return true", function () {
+    const slot = '0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF'
+    const { counter, start } = getBlockIntervalState({ slot })
+    expect(counter).to.equal(0xFFFF)
+    expect(start).to.equal(0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF) 
+  })
+
+  it("Should return 0xFF for counter and 0xFFF for start", function () {
+    const slot = '0x000000000000000000000000000000FF00000000000000000000000000000FFF'
+    const { counter, start } = getBlockIntervalState({ slot })
+    expect(counter).to.equal(0xFF)
+    expect(start).to.equal(0xFFF) 
   })
 })
