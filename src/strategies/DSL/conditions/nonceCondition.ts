75002b3cf3cfbd65dcaee2f9f781a00eb36d8eac
Phanarat Pak <phanpak@gmail.com>
2023-09-21 15:50:56 +0200

    Fix parameters for the primitive

--- src/strategies/DSL/conditions/nonceCondition.ts
+++ src/strategies/DSL/conditions/nonceCondition.ts
@@ -1,5 +1,5 @@
 import { PrimitiveArgs } from '@brinkninja/types'
-import { NonceConditionArgs, nonceToBit } from '@brink-sdk'
+import { NonceConditionArgs, nonceToBit, BitJSON } from '@brink-sdk'
 
 const STATE_USED = 'USED'
 const STATE_NOT_USED = 'NOT_USED'
@@ -7,25 +7,21 @@ const STATE_NOT_USED = 'NOT_USED'
 function nonceCondition ({state, nonce} : NonceConditionArgs): PrimitiveArgs[] {
   const bit = nonceToBit({nonce})
 
+  const bitJson: BitJSON = { index: bit.index.toString(), value: bit.value.toString() }
+
   switch (state) {
     case STATE_USED:
       return [
         {
           functionName: 'requireBitUsed',
-          params: {
-            bitmapIndex: bit.index,
-            bit: bit.value,
-          }
+          params: { bit: bitJson },
         }
       ]
     case STATE_NOT_USED:
       return [
         {
           functionName: 'requireBitNotUsed',
-          params: {
-            bitmapIndex: bit.index,
-            bit: bit.value,
-          }
+          params: { bit: bitJson }
         }
       ]
     default:
