1af76871d05dcd47baa8c55fa88c1494d1f0b65e
Phanarat Pak <phanpak@gmail.com>
2023-09-25 16:13:43 +0200

    Add decimals

--- src/strategies/DSL/conditions/priceCondition.ts
+++ src/strategies/DSL/conditions/priceCondition.ts
@@ -1,5 +1,5 @@
-import { PrimitiveArgs, GREATER_THAN_OPERATOR, LESS_THAN_OPERATOR } from '@brinkninja/types'
-import { PriceConditionArgs, UniV3Twap, Token } from '@brink-sdk'
+import { PrimitiveArgs, GREATER_THAN_OPERATOR, LESS_THAN_OPERATOR, BigIntish } from '@brinkninja/types'
+import { PriceConditionArgs, UniV3Twap, Token, priceToTwapValue } from '@brink-sdk'
 import { toTokenArgs } from '@brink-sdk/internal/toTokenArgs';
 import { FeeAmount } from '@uniswap/v3-sdk'
 
@@ -9,16 +9,18 @@ const DEFAULT_TIME_INTERVAL = BigInt(1000)
 function priceCondition ({
   operator,
   tokenA,
+  tokenA_decimals,
   tokenB,
+  tokenB_decimals,
   price,
-  twapInterval,
-  twapFeePool
+  twapInterval = DEFAULT_TIME_INTERVAL,
+  twapFeePool,
 }: PriceConditionArgs): PrimitiveArgs[] {
   const twap = new UniV3Twap({
     tokenA: new Token(toTokenArgs(tokenA)),
     tokenB: new Token(toTokenArgs(tokenB)),
-    interval: twapInterval || DEFAULT_TIME_INTERVAL,
-    fee: twapFeePool as FeeAmount
+    interval: twapInterval,
+    fee: BigIntishToFeeAmount(twapFeePool),
   })
 
   const oracle = {
@@ -26,13 +28,19 @@ function priceCondition ({
     params: twap.params,
   }
 
+  const twapValue = priceToTwapValue({
+    price,
+    tokenA_decimals,
+    tokenB_decimals,
+  })
+
   switch (operator) {
     case LESS_THAN_OPERATOR:
     return [{
       functionName: 'requireUint256LowerBound', 
       params: {
         oracle: oracle,
-        lowerBound: price,
+        lowerBound: twapValue,
       }
     }]
     case GREATER_THAN_OPERATOR:
@@ -40,12 +48,24 @@ function priceCondition ({
         functionName: 'requireUint256UpperBound',
         params: {
           oracle: oracle,
-          upperBound: price,
+          upperBound: twapValue,
         }
       }]
     default:
       throw new Error(`Operator ${operator} is not valid`)
   }
+}
+
+function BigIntishToFeeAmount(value: BigIntish | undefined): FeeAmount | undefined {
+    if (value === undefined) {
+        return undefined;
+    }
+
+    if (Object.values(FeeAmount).includes(Number(value))) {
+        return value as FeeAmount;
+    }
+
+    throw new Error('Invalid fee amount');
 }
 
 export default priceCondition
--- test/local/DSL/priceCondition.test.ts
+++ test/local/DSL/priceCondition.test.ts
@@ -1,22 +1,28 @@
 import { expect } from 'chai'
-import { priceCondition, Token, UniV3Twap } from '@brink-sdk'
+import { priceCondition, priceToTwapValue, Token, UniV3Twap } from '@brink-sdk'
 
 describe('priceCondition', function () {
   const USDC_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
   const DAI_ADDRESS = '0x6b175474e89094c44da98b954eedeac495271d0f'
 
+  const tokenArgs = {
+      tokenA: USDC_ADDRESS,
+      tokenA_decimals: 2,
+      tokenB: DAI_ADDRESS,
+      tokenB_decimals: 2,
+  }
+
   it('should return requireUint256LowerBound primitive when operator is lt', function () {
     const primitives = priceCondition({
+      ...tokenArgs,
       type: 'price',
       operator: 'lt',
-      tokenA: USDC_ADDRESS,
-      tokenB: DAI_ADDRESS,
       price: 1000
     })
 
     expect(primitives.length).to.equal(1)
     expect(primitives[0].functionName).to.equal('requireUint256LowerBound')
-    expect(primitives[0].params.lowerBound).to.equal(1000)
+    expect(primitives[0].params.lowerBound).to.equal(getTwapValue(1000))
 
     const twap = new UniV3Twap({
       tokenA: new Token({address: USDC_ADDRESS}),
@@ -29,16 +35,15 @@ describe('priceCondition', function () {
 
   it('should return requireUint256UpperBound primitive when operator is gt', function () {
     const primitives = priceCondition({
+      ...tokenArgs,
       type: 'price',
       operator: 'gt',
-      tokenA: USDC_ADDRESS,
-      tokenB: DAI_ADDRESS,
       price: 2000
     })
 
     expect(primitives.length).to.equal(1)
     expect(primitives[0].functionName).to.equal('requireUint256UpperBound')
-    expect(primitives[0].params.upperBound).to.equal(2000)
+    expect(primitives[0].params.upperBound).to.equal(getTwapValue(2000))
 
     const twap = new UniV3Twap({
       tokenA: new Token({address: USDC_ADDRESS}),
@@ -48,4 +53,12 @@ describe('priceCondition', function () {
 
     expect(primitives[0].params.oracle).to.deep.equal({ address: twap.address, params: twap.params })
   })
+
+  function getTwapValue(price: number): bigint {
+    return priceToTwapValue({
+      price: price,
+      tokenA_decimals: tokenArgs.tokenA_decimals,
+      tokenB_decimals: tokenArgs.tokenB_decimals,
+    })
+  }
 })
