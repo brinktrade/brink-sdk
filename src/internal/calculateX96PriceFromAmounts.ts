834a0ff72b00701e9649d8588f28c8c697715501
Phanarat Pak <phanpak@gmail.com>
2023-09-29 14:02:55 +0200

    Rename to quoteAmount and baseAmount

--- src/internal/calculateX96PriceFromAmounts.ts
+++ src/internal/calculateX96PriceFromAmounts.ts
@@ -1,7 +1,7 @@
-export function calculateX96PriceFromAmounts(amount0: bigint, amount1: bigint): bigint {
-  if(amount1 === 0n) {
+export function calculateX96PriceFromAmounts(quoteAmount: bigint, baseAmount: bigint): bigint {
+  if(baseAmount === 0n) {
     throw new Error("Division by zero");
   }
 
-  return (amount0 * (2n ** 96n)) / amount1;
+  return (quoteAmount * (2n ** 96n)) / baseAmount;
 }
--- src/internal/convertToX96HexPrice.ts
+++ src/internal/convertToX96HexPrice.ts
@@ -2,7 +2,7 @@ import { bigintToHex } from "./bigintToHex";
 import { calculateX96PriceFromAmounts } from "./calculateX96PriceFromAmounts";
 
 
-export function convertToX96HexPrice(tokenOutAmount: bigint, tokenInAmount: bigint): string {
-    const x96Price = calculateX96PriceFromAmounts(tokenOutAmount, tokenInAmount);
+export function convertToX96HexPrice(quoteAmount: bigint, baseAmount: bigint): string {
+    const x96Price = calculateX96PriceFromAmounts(quoteAmount, baseAmount);
     return bigintToHex(x96Price);
 }
