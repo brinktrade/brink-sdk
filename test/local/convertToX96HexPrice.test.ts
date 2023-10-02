3dce4e46f48f5c53e3d8c296e1bd6d2246d5e148
Phanarat Pak <phanpak@gmail.com>
2023-10-02 10:15:09 +0200

    Remove top level description for test

--- test/local/convertToX96HexPrice.test.ts
+++ test/local/convertToX96HexPrice.test.ts
@@ -1,47 +1,43 @@
 import { convertToX96HexPrice } from '@brink-sdk/internal';
 import { expect } from 'chai';
 
-describe('Utils', () => {
+describe('convertToX96HexPrice', () => {
+  // Simple Cases
+  it('should convert 200/100 correctly', () => {
+    const result = convertToX96HexPrice(100n, 200n);
+    // 200 divided by 100 is 2. Then 2 multiplied by 2^96 is a known bigint, converted to hex
+    expect(result).to.equal('0x2000000000000000000000000'); 
+  });
 
-  describe('convertToX96HexPrice', () => {
-    
-    // Simple Cases
-    it('should convert 200/100 correctly', () => {
-      const result = convertToX96HexPrice(100n, 200n);
-      // 200 divided by 100 is 2. Then 2 multiplied by 2^96 is a known bigint, converted to hex
-      expect(result).to.equal('0x2000000000000000000000000'); 
-    });
+  it('should convert 300/100 correctly', () => {
+    const result = convertToX96HexPrice(100n, 300n);
+    // 300 divided by 100 is 3. Then 3 multiplied by 2^96 is a known bigint, converted to hex
+    expect(result).to.equal('0x3000000000000000000000000');
+  });
 
-    it('should convert 300/100 correctly', () => {
-      const result = convertToX96HexPrice(100n, 300n);
-      // 300 divided by 100 is 3. Then 3 multiplied by 2^96 is a known bigint, converted to hex
-      expect(result).to.equal('0x3000000000000000000000000');
-    });
+  // Edge Cases
+  it('should convert 1/1 correctly', () => {
+    const result = convertToX96HexPrice(1n, 1n);
+    // 1 divided by 1 is 1. Then 1 multiplied by 2^96 is 2^96, converted to hex
+    expect(result).to.equal('0x1000000000000000000000000');
+  });
 
-    // Edge Cases
-    it('should convert 1/1 correctly', () => {
-      const result = convertToX96HexPrice(1n, 1n);
-      // 1 divided by 1 is 1. Then 1 multiplied by 2^96 is 2^96, converted to hex
-      expect(result).to.equal('0x1000000000000000000000000');
-    });
+  it('should convert 200/1 correctly', () => {
+    const result = convertToX96HexPrice(1n, 200n);
+    // 200 multiplied by 2^96 is a known bigint, converted to hex
+    expect(result).to.equal('0xc8000000000000000000000000');
+  });
 
-    it('should convert 200/1 correctly', () => {
-      const result = convertToX96HexPrice(1n, 200n);
-      // 200 multiplied by 2^96 is a known bigint, converted to hex
-      expect(result).to.equal('0xc8000000000000000000000000');
-    });
+  // Big Number Handling
+  it('should handle big numbers correctly', () => {
+    const bigNumber = 10n**12n; // a trillion
+    const result = convertToX96HexPrice(1n, bigNumber);
+    const expectedValue = BigInt(bigNumber) * BigInt(2**96);
+    expect(result).to.equal('0x' + expectedValue.toString(16));
+  });
 
-    // Big Number Handling
-    it('should handle big numbers correctly', () => {
-      const bigNumber = 10n**12n; // a trillion
-      const result = convertToX96HexPrice(1n, bigNumber);
-      const expectedValue = BigInt(bigNumber) * BigInt(2**96);
-      expect(result).to.equal('0x' + expectedValue.toString(16));
-    });
-
-    // Division by zero
-    it('should throw error for division by zero', () => {
-      expect(() => convertToX96HexPrice(0n, 200n)).to.throw("Division by zero");
-    });
+  // Division by zero
+  it('should throw error for division by zero', () => {
+    expect(() => convertToX96HexPrice(0n, 200n)).to.throw("Division by zero");
   });
 });
