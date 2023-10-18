ee312ec1e7903ca82a810c60a63139b97902d0cc
Phanarat Pak <phanpak@gmail.com>
2023-10-18 21:14:36 +0200

    Remove comment

--- src/internal/joiExtended.ts
+++ src/internal/joiExtended.ts
@@ -19,60 +19,45 @@ export const joi = Joi
     };
   })
 
-  // // validates bigintish
-  // .extend((joi: Joi.Root) => {
-  //   return {
-  //     base: joi.alternatives().try(joi.string(), joi.number()),
-  //     messages: {
-  //       'bigIntish.base': '{{#value}} is not a valid bigIntish'
-  //     },
-  //     type: 'bigIntish',
-  //     validate (value: any, helpers: any) {
-  //       if (!isBigIntish(value)) {
-  //         return { errors: helpers.error('bigIntish.base'), value, };
-  //       }
-  //     }
-  //   };
-  // })
-.extend((joi: Joi.Root) => {
-  return {
-    base: joi.alternatives().try(joi.string(), joi.number()),
-    messages: {
-      'bigIntish.base': '{{#value}} is not a valid bigIntish',
-      'bigIntish.min': 'min not satisfied'
-    },
-    type: 'bigIntish',
-    validate (value: any, helpers: any) {
-      if (!isBigIntish(value)) {
-        return { errors: helpers.error('bigIntish.base'), value, };
-      }
-    },
-    rules: {
-      min: {
-        method(q: any): any {
-          // @ts-ignore: Unreachable code error
-          return this.$_addRule({ name: 'min', args: { q } });
-        },
-        args: [
-          {
-            name: 'q',
-            ref: true,
-            // should validate that q is a bigIntish. should convert to big int and compare to the arg value
-            assert: (value: any) => isBigIntish(value),
-            message: 'must be a number or string representing an integer'
-          }
-        ],
-        validate (value: any, helpers: any, args: any, options: any) {
-          if (BigInt(value) < BigInt(args.q)) {
-            return helpers.error('bigIntish.min', { q: args.q });
-          }
+  .extend((joi: Joi.Root) => {
+    return {
+      base: joi.alternatives().try(joi.string(), joi.number()),
+      messages: {
+        'bigIntish.base': '{{#value}} is not a valid bigIntish',
+        'bigIntish.min': 'min not satisfied'
+      },
+      type: 'bigIntish',
+      validate (value: any, helpers: any) {
+        if (!isBigIntish(value)) {
+          return { errors: helpers.error('bigIntish.base'), value, };
+        }
+      },
+      rules: {
+        min: {
+          method(q: any): any {
+            // @ts-ignore: Unreachable code error
+            return this.$_addRule({ name: 'min', args: { q } });
+          },
+          args: [
+            {
+              name: 'q',
+              ref: true,
+              // should validate that q is a bigIntish. should convert to big int and compare to the arg value
+              assert: (value: any) => isBigIntish(value),
+              message: 'must be a number or string representing an integer'
+            }
+          ],
+          validate (value: any, helpers: any, args: any, options: any) {
+            if (BigInt(value) < BigInt(args.q)) {
+              return helpers.error('bigIntish.min', { q: args.q });
+            }
 
-          return value
+            return value
+          }
         }
       }
-    }
-  };
-})
+    };
+  })
 
 
   // // validates positive bigintish
