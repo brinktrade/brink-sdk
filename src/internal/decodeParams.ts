339c97b5dc8117988f4ce1f0931cb0a2a4533323
Mike Calvanese <mfcalvanese@gmail.com>
2023-12-05 23:31:22 -0500

    add tests and error handling for encode and decode params fns

--- src/internal/decodeParams.ts
+++ src/internal/decodeParams.ts
@@ -6,8 +6,13 @@ const abiCoder = ethers.utils.defaultAbiCoder
 function decodeParams (
   { data, paramTypes }: { data: string, paramTypes: SolidityFunctionParamType[] }
 ): ContractCallParam[] {
-  const decodedParams = abiCoder.decode(paramTypes.map(v => v.type), data)
-  return decodedParams.map(v =>  v.toString())
+  let decodedParams
+  try {
+    decodedParams = abiCoder.decode(paramTypes.map(v => v.type), data)
+  } catch (err: any) {
+    throw new Error(`Error decoding params data for types ${JSON.stringify(paramTypes)}: ${err.message}`)
+  }
+  return decodedParams.map(v =>  v.toString().toLowerCase())
 }
 
 export default decodeParams
--- src/internal/encodeParams.ts
+++ src/internal/encodeParams.ts
@@ -6,7 +6,13 @@ const abiCoder = ethers.utils.defaultAbiCoder
 function encodeParams (
   { params, paramTypes }: { params: ContractCallParam[], paramTypes: SolidityFunctionParamType[] }
 ): string {
-  return abiCoder.encode(paramTypes.map(v => v.type), params)
+  let data: string
+  try {
+    data = abiCoder.encode(paramTypes.map(v => v.type), params)
+  } catch (err: any) {
+    throw new Error(`Error encoding params [${params}]: ${err.message}`)
+  }
+  return data
 }
 
 export default encodeParams
